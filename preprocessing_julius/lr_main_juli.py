a61dc640162b7529a91e068901ec3ec9f980ddd0
Julius Richardt <juliusrichardt.jr@gmail.com>
2025-04-12 14:41:39 +0200

    preprocessing_juli geadded

---
+++ preprocessing_julius/lr_main_juli.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.linear_model import LinearRegression
+
+import gc  # Garbage Collector zur Speicherverwaltung
+from preprocessing_juli import preprocessing_pipeline
+from eval_call_juli import evaluate_model
+
+
+
+def main():
+    X_train, X_test, y_train, y_test , X,y, categorical_features , numeric_features = preprocessing_pipeline()
+
+    # Preprocessing-Pipelines erstellen
+    numeric_transformer = Pipeline(steps=[
+        ('imputer', SimpleImputer(strategy='median')),
+        ('scaler', StandardScaler())
+    ])
+
+    categorical_transformer = Pipeline(steps=[
+        ('imputer', SimpleImputer(strategy='most_frequent')),
+        ('onehot', OneHotEncoder(handle_unknown='ignore'))
+    ])
+
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ('num', numeric_transformer, numeric_features),
+            ('cat', categorical_transformer, categorical_features)
+        ])
+
+
+    linear_regression_pipeline = Pipeline(steps=[
+        ('preprocessor', preprocessor),
+        ('model', LinearRegression())
+    ])
+
+    # Modell trainieren
+    linear_regression_pipeline.fit(X_train, y_train)
+
+    # Vorhersagen treffen
+    y_pred_lr = linear_regression_pipeline.predict(X_test)
+
+
+
+    evaluate_model(y_test, y_pred_lr, "Linear Regression")
+
+
+
+if __name__ == "__main__":
+    main()
--- preprocessing_julius/preprocessing_juli.py
+++ preprocessing_julius/preprocessing_juli.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,7 +8,51 @@ from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+import re
 import gc  # Garbage Collector zur Speicherverwaltung
+
+
+
+def fix_model_brand_conflicts(df):
+    '''Diese Funktion überprüft, ob es Zeilen gibt, in denen brand = model ist. In diesen Zeilen haben wir keine Informationen
+    über das Model. 
+    
+    Um die Zeilen aber nicht direkt zu droppen, wird vorher geschaut, ob man über bestimmte Spalten das Model eindeutig zuornden kann.
+    Ist eine eindeutige Zuordnung möglich, dann überschreiben wir die ursprüngliche Ausprägung in model. Ist keine Zuordnung möglich, dann wird die Zeile
+    gedroppt.
+    '''
+
+    def normalize(text):
+        if pd.isna(text):
+            return ""
+        return re.sub(r'[^a-z0-9]', '', text.lower())
+
+    df['brand_norm'] = df['brand'].apply(normalize)
+    df['model_norm'] = df['model'].apply(normalize)
+
+    mask_same = df['brand_norm'] == df['model_norm']
+
+    problem_rows = df[mask_same].copy()
+    clean_rows = df[~mask_same].copy()
+
+    grouped_models = clean_rows \
+        .groupby(['brand', 'power_ps', 'fuel_consumption_g_km', 'transmission_type', 'fuel_type'])['model'] \
+        .unique().reset_index() 
+
+    grouped_models = grouped_models[grouped_models['model'].apply(len) == 1] # nur kontexte bei denen model unique ist (ein element in der liste)
+    grouped_models['model'] = grouped_models['model'].apply(lambda x: x[0]) # nimm nur das erste element aus der liste
+
+    problem_fixed = problem_rows.merge(grouped_models, on=['brand', 'power_ps', 'fuel_consumption_g_km', 'transmission_type', 'fuel_type'],
+                                       how='left', suffixes=('', '_fixed'))
+
+    recovered = problem_fixed[problem_fixed['model_fixed'].notna()].copy()
+    recovered['model'] = recovered['model_fixed']
+    recovered = recovered.drop(columns=['model_fixed'])
+
+    final_df = pd.concat([clean_rows, recovered], ignore_index=True) \
+                 .drop(columns=['brand_norm', 'model_norm'])
+
+    return final_df
 
 
 def preprocessing_pipeline():
@@ -22,6 +65,8 @@ def preprocessing_pipeline():
         df = df.drop('Unnamed: 0', axis=1)
     if 'offer_description' in df.columns:
         df = df.drop('offer_description', axis=1)
+
+    df = df.drop_duplicates()
 
     # Filtere die Jahr-Spalte (Achtung: Dieser Filter nimmt die ersten 29 eindeutigen Werte; evtl. anpassen)
     print('Werte in der year-Spalte vor dem Filtern:')
@@ -70,10 +115,33 @@ def preprocessing_pipeline():
     df['mileage_in_km'] = pd.to_numeric(df['mileage_in_km'], errors='coerce')
     df['price_in_euro'] = pd.to_numeric(df['price_in_euro'], errors='coerce')
 
+    # Fixe wo model = brand, versuche eindeutig Model zuzuweisen sonst droppen 
+    df = fix_model_brand_conflicts(df)
+
+    # Model war hier in einer Zeile falsch geschrieben
+    df['model'] = df['model'].replace('Saab 93', 'Saab 9-3')
+
+    # drop power_kw da Vielfaches von power_ps
+    if 'power_kw' in df.columns:
+        df = df.drop('power_kw', axis=1)
+    
+
     # Entferne Zeilen mit fehlenden Werten
     print(f"Dataset-Größe vor dem Entfernen von NaN-Werten: {len(df)}")
     df = df.dropna()
     print(f"Dataset-Größe nach dem Entfernen von NaN-Werten: {len(df)}")
+
+
+    def filter_models_with_min_count(df, model_col='model', min_count=100):
+        '''Diese Funktion filtert den DF auf die Modelle ein die in einer bestimmten
+        Frequenz vorkommen'''
+        model_counts = df[model_col].value_counts()
+        models_over_threshold = model_counts[model_counts >= min_count].index
+        df_filtered = df[df[model_col].isin(models_over_threshold)]
+        return df_filtered
+    
+    # Nur die Modelle, die mind. 100 mal vorkommen
+    df = filter_models_with_min_count(df, 'model', 100)
 
     # Definiere das Ziel
     y = df['price_in_euro']
@@ -89,5 +157,6 @@ def preprocessing_pipeline():
     gc.collect()
 
     # Datensatz in Trainings- und Testdaten aufteilen
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=X['model']) #stratify auf basis von model spalte
     return X_train, X_test, y_train, y_test , X,y, categorical_features , numeric_features
+
