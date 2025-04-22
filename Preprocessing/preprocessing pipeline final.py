039f4098a273f86d3c60fd84075d5f7005cc734a
Valerie2009 <57894801+Valerie2009@users.noreply.github.com>
2025-04-22 21:00:03 +0200

    Update preprocessing pipeline final.py

--- Preprocessing/preprocessing pipeline final.py
+++ Preprocessing/preprocessing pipeline final.py
@@ -188,5 +188,5 @@ def preprocessing_pipeline():
     gc.collect()
 
     # Datensatz in Trainings- und Testdaten aufteilen
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=X['model'])
     return X_train, X_test, y_train, y_test , X,y, categorical_features , numeric_features
