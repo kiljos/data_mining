d4d79c358fd0637c1172366d3a8e551a98b4a3ad
Julius Richardt <juliusrichardt.jr@gmail.com>
2025-05-03 11:34:15 +0200

    anpassungen

--- Preprocessing/split.py
+++ Preprocessing/split.py
@@ -1,7 +1,7 @@
 
 from sklearn.model_selection import train_test_split
 
-def split_data(df, col_to_stratify_on = 'model'):
+def split_data(df):
     # Zielvariable festlegen       
     y = df['price_in_euro']
     X = df.drop(['price_in_euro'], axis=1)
@@ -11,5 +11,5 @@ def split_data(df, col_to_stratify_on = 'model'):
     categorical_features = X.select_dtypes(include=['object']).columns.tolist()
 
     # Datensatz in Trainings- und Testdaten aufteilen
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=X[col_to_stratify_on])
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     return X_train, X_test, y_train, y_test , X,y, categorical_features , numeric_features
