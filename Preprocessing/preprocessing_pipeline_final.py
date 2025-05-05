340e9242250e48b77d549a9c9e9b707d347c2240
VictoriaLahr <118441010+VictoriaLahr@users.noreply.github.com>
2025-05-05 18:17:37 +0200

    Separated Offer_Description
    ! Wer die Pipeline importiert, muss in preprocessing_pipeline_initial rauslöschen, dass offer_description gedropped wird. Aus Laufzeitgründen lass ich das vorerst noch drin

--- Preprocessing/preprocessing_pipeline_final.py
+++ Preprocessing/preprocessing_pipeline_final.py
@@ -187,76 +187,7 @@ def preprocessing_pipeline():
         return df_filtered
     
     # Nur die Modelle, die mind. 100 mal vorkommen
-    df = filter_models_with_min_count(df, 'model', 100)
-    # Auswerten der Offer Description Spalte
-
-    """ 
-    Kurz zusammengefasst: Mit dem SkLearn Count Vectorizer werden die 50 häufigsten Wörter in der Spalte Offer Description
-    gezählt. Weil viele davon inhaltlich irgendwie zusammengehören, z.B. Quattro, xDrive, all-wheel-drive und einige auch 
-    nicht sinnvoll miteingebunden werden können, z.B. das Wort "Hand" wegen 2., 3. 4. Hand, oder "auto", wurden diese Wörter
-    extrahiert und in einer .csv datei kategorisiert. 
-    Diese .csv Datei namens "offer_description_utf8.csv" wird dann wieder importiert und daraus ein dictionary erstellt. 
-    Mit Hilfe dieses Dictionaries wird dann für jede Kategorie eine neue Spalte erstellt und mit Yes / No ausgefüllt.
-    z.B. Neue Spalte "has_navi": Yes, wenn in der Spalte offer description "navi" gefunden wurde, sonst No
-    (insgesamt 17 Kategorien, also 17 neue Spalten)
-    """
-    from sklearn.feature_extraction.text import CountVectorizer
-
-    # Liste von deutschen Stopwörter, die nicht mitgezählt werden sollen (generiert von ChatGPT)
-    german_stopwords = [
-    "aber", "abgesehen", "alle", "allem", "allen", "alles", "als", "also", "am", "an", "andere", "anderen", "anderm", 
-    "anderer", "anderes", "auch", "auf", "aus", "bei", "beide", "beiden", "beim", "beiner", "beim", "bis", "bisschen", 
-    "bleiben", "durch", "ein", "eine", "einem", "einen", "einer", "eines", "er", "es", "essen", "etwas", "für", "gegen", 
-    "geht", "habe", "haben", "hat", "hatte", "hätten", "hattest", "hattete", "hier", "hin", "hinter", "ich", "ihn", "ihm", 
-    "ihr", "ihre", "immer", "in", "indem", "ins", "irgendwas", "ist", "ja", "jeder", "jeden", "jedes", "jedoch", "kann", 
-    "kein", "keine", "konnte", "konnte", "machen", "mehr", "mit", "muss", "muss", "nach", "nicht", "nichts", "nur", "ob", 
-    "oder", "ohne", "sehr", "sich", "sie", "sind", "so", "sollen", "sollte", "sondern", "um", "und", "uns", "unsere", 
-    "vom", "von", "vor", "während", "weil", "wer", "werden", "wie", "wieder", "wieso", "wir", "wird", "wirst", "wo", 
-    "woher", "wohin", "zu", "zum", "zur", "zwar", "zwischen"
-    ]
-
-    # Entferne Sonderzeichen und Zahlen (weil zu unaussagekräftig) und wandle alles in Kleinbuchstaben um
-    # Wird in die neue Spalte _cleaned geschrieben
-    df["offer_description_cleaned"] = (
-    df["offer_description"]
-    .str.replace(r'\d+', '', regex=True)             # Entfernt alle Zahlen
-    .str.replace(r'[^\w\s]', '', regex=True)         # Entfernt Sonderzeichen
-    .str.lower()                                     # Alles klein
-    )
-
-    #Sollten eigentlich schon draußen sein, aber entfernt zur sicherheit nochmal alle leeren werte 
-    df = df.dropna(subset=["offer_description_cleaned"]) 
-
-    # Erstellen eines CountVectorizer, der die deutschen Stopwörter verwendet -> nimmt sich hier jetzt die 50 häufigsten wörter
-    vectorizer = CountVectorizer(analyzer='word', stop_words=german_stopwords, max_features=50)
-
-    # Anwendung des Vectorizers
-    X = vectorizer.fit_transform(df["offer_description_cleaned"])
-
-    # Erstellen eines DataFrame, um die Häufigkeit der Wörter zu sehen
-    word_freq = pd.DataFrame(X.toarray(), columns=vectorizer.get_feature_names_out())
-
-    # Berechnung der Häufigkeit jedes Schlagworts
-    word_count = word_freq.sum().sort_values(ascending=False)
-
-
-
-    # Die oben ausgegebene Liste an häufigsten Wörtern wurde dann per Hand aussortiert, e.g. Wörter wie "auto, matic" usw. wurden entfernt
-    # Und ähnliche Wörter in Kategorien zusammengefasst z.B. "quattro, xDrive und awd = all wheel drive"
-    # Einlesen der Kategorien aus der csv-datei "offer_description_utf8.csv"
-    kat = pd.read_csv("offer_description_utf8.csv", sep=";")
-    # Werte ohne zugeordnete Kategorien werden gelöscht
-    kat = kat.dropna(subset=["Kategorie"])
-
-    # Kategorien + zugehörige Abkürzungen in Dictionary gruppieren
-    abkuerzung_dict = kat.groupby("Kategorie")["Abkuerzung"].apply(list).to_dict()
-
-    # Für jede Kategorie eine neue Spalte erzeugen, die boolesche Werte enthält
-    # Also z.B. has_navigation? yes / no
-    for kategorie, abkuerzungen in abkuerzung_dict.items():
-        df[kategorie] = df["offer_description"].apply(
-        lambda text: True if any(abk.lower() in text.lower() for abk in abkuerzungen) else False
-        )
+    df = filter_models_with_min_count(df, 'model', 100)   
             
      # Zielvariable festlegen       
     y = df['price_in_euro']
---
+++ Preprocessing/preprocessing_pipeline_offerdesc.py
@@ -0,0 +1,61 @@
+import pandas as pd
+
+def preprocessing_pipeline_offerdesc(df):
+
+    from sklearn.feature_extraction.text import CountVectorizer
+
+    # Liste von deutschen Stopwörter, die nicht mitgezählt werden sollen (generiert von ChatGPT)
+    german_stopwords = [
+    "aber", "abgesehen", "alle", "allem", "allen", "alles", "als", "also", "am", "an", "andere", "anderen", "anderm", 
+    "anderer", "anderes", "auch", "auf", "aus", "bei", "beide", "beiden", "beim", "beiner", "beim", "bis", "bisschen", 
+    "bleiben", "durch", "ein", "eine", "einem", "einen", "einer", "eines", "er", "es", "essen", "etwas", "für", "gegen", 
+    "geht", "habe", "haben", "hat", "hatte", "hätten", "hattest", "hattete", "hier", "hin", "hinter", "ich", "ihn", "ihm", 
+    "ihr", "ihre", "immer", "in", "indem", "ins", "irgendwas", "ist", "ja", "jeder", "jeden", "jedes", "jedoch", "kann", 
+    "kein", "keine", "konnte", "konnte", "machen", "mehr", "mit", "muss", "muss", "nach", "nicht", "nichts", "nur", "ob", 
+    "oder", "ohne", "sehr", "sich", "sie", "sind", "so", "sollen", "sollte", "sondern", "um", "und", "uns", "unsere", 
+    "vom", "von", "vor", "während", "weil", "wer", "werden", "wie", "wieder", "wieso", "wir", "wird", "wirst", "wo", 
+    "woher", "wohin", "zu", "zum", "zur", "zwar", "zwischen"
+    ]
+
+    # Entferne Sonderzeichen und Zahlen (weil zu unaussagekräftig) und wandle alles in Kleinbuchstaben um
+    # Wird in die neue Spalte _cleaned geschrieben
+    df["offer_description_cleaned"] = (
+    df["offer_description"]
+    .str.replace(r'\d+', '', regex=True)             # Entfernt alle Zahlen
+    .str.replace(r'[^\w\s]', '', regex=True)         # Entfernt Sonderzeichen
+    .str.lower()                                     # Alles klein
+    )
+
+    #Sollten eigentlich schon draußen sein, aber entfernt zur sicherheit nochmal alle leeren werte 
+    df = df.dropna(subset=["offer_description_cleaned"]) 
+
+    # Erstellen eines CountVectorizer, der die deutschen Stopwörter verwendet -> nimmt sich hier jetzt die 50 häufigsten wörter
+    vectorizer = CountVectorizer(analyzer='word', stop_words=german_stopwords, max_features=50)
+
+    # Anwendung des Vectorizers
+    X = vectorizer.fit_transform(df["offer_description_cleaned"])
+
+    # Erstellen eines DataFrame, um die Häufigkeit der Wörter zu sehen
+    word_freq = pd.DataFrame(X.toarray(), columns=vectorizer.get_feature_names_out())
+
+    # Berechnung der Häufigkeit jedes Schlagworts
+    word_count = word_freq.sum().sort_values(ascending=False)
+
+    # Die oben ausgegebene Liste an häufigsten Wörtern wurde dann per Hand aussortiert, e.g. Wörter wie "auto, matic" usw. wurden entfernt
+    # Und ähnliche Wörter in Kategorien zusammengefasst z.B. "quattro, xDrive und awd = all wheel drive"
+    # Einlesen der Kategorien aus der csv-datei "offer_description_utf8.csv"
+    kat = pd.read_csv("offer_description_utf8.csv", sep=";")
+    # Werte ohne zugeordnete Kategorien werden gelöscht
+    kat = kat.dropna(subset=["Kategorie"])
+
+    # Kategorien + zugehörige Abkürzungen in Dictionary gruppieren
+    abkuerzung_dict = kat.groupby("Kategorie")["Abkuerzung"].apply(list).to_dict()
+
+    # Für jede Kategorie eine neue Spalte erzeugen, die boolesche Werte enthält
+    # Also z.B. has_navigation? yes / no
+    for kategorie, abkuerzungen in abkuerzung_dict.items():
+        df[kategorie] = df["offer_description"].apply(
+        lambda text: True if any(abk.lower() in text.lower() for abk in abkuerzungen) else False
+        )
+
+    return df
