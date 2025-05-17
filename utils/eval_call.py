33b890dfebe932b5ba10a29ef725357127da6e7c
Julius Richardt <juliusrichardt.jr@gmail.com>
2025-05-17 17:29:00 +0200

    commit

--- utils/eval_call.py
+++ utils/eval_call.py
@@ -3,17 +3,23 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import pandas as pd
 import numpy as np
 
+def mean_absolute_percentage_error(y_true, y_pred):
+    y_true, y_pred = np.array(y_true), np.array(y_pred)
+    nonzero_idx = y_true != 0
+    return np.mean(np.abs((y_true[nonzero_idx] - y_pred[nonzero_idx]) / y_true[nonzero_idx])) * 100
 
 def evaluate_model(y_true, y_pred, model_name):
     """Evaluate model performance using various metrics."""
     mae = mean_absolute_error(y_true, y_pred)
     mse = mean_squared_error(y_true, y_pred)
+    mape = mean_absolute_percentage_error(y_true, y_pred)
     rmse = np.sqrt(mse)
     r2 = r2_score(y_true, y_pred)
     
     print(f"{model_name} Performance Metrics:")
     print(f"MAE: {mae:.2f}")
-    print(f"MSE: {mse:.2f}")
+    #print(f"MSE: {mse:.2f}")
+    print(f"MAPE: {mape:.2f}")
     print(f"RMSE: {rmse:.2f}")
     print(f"R²: {r2:.2f}")
     print("-" * 30)
